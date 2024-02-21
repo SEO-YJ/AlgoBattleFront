cf387b08c229657b444f211ba96084230949c71a
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-02-21 15:13:32 +0900

    refactor: 라우터 통합

--- src/routers/main-router.jsx
+++ src/routers/main-router.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import TotalLayout from '../components/TotalLayout'
-import { createBrowserRouter } from 'react-router-dom'
-import MainPage from '../routes/main/page';
-import RoomPage from '../routes/room/page';
+import React from "react";
+import TotalLayout from "../components/TotalLayout";
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "../routes/main/page";
+import RoomPage from "../routes/room/page";
+import GamePage from "~/routes/game/page";
 
 export const mainRouter = [
   {
     path: "/",
-    element: <TotalLayout/>,
+    element: <TotalLayout />,
     children: [
       {
         path: "/",
-        element: <MainPage/>,
-        index : true,
+        element: <MainPage />,
+        index: true,
       },
       {
         path: "/room",
-        element: <RoomPage/>,
-        index : true,
+        element: <RoomPage />,
+        index: true,
       },
-    ]
+      {
+        path: "/room/game",
+        element: <GamePage />,
+        index: true,
+      },
+    ],
   },
 ];
 
--- src/routes/room/page.jsx
+++ src/routes/room/page.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from "react";
 
-//여기서부터 시작하면 됨. 이름은 바꿔도 무방
 export default function RoomPage() {
-  return (
-    <div>RoomPage</div>
-  )
+  return <div>RoomPage</div>;
 }
