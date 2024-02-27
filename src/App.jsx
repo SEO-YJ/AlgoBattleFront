29625fdac163e8a111a81027dc313cb8b40d9142
kim hyun soo <hyjkl0124@naver.com>
2024-02-27 09:02:47 +0900

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { RouterProvider } from "react-router-dom"
-import router from "./routers/main-router"
-import { Provider } from "react-redux"
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/main-router";
+import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./routes/store"
+import store, { persistor } from "./routes/store";
 
 function App() {
   return (
     <>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <RouterProvider router={router}/>
+          <RouterProvider router={router} />
         </PersistGate>
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes/main/page.jsx
+++ src/routes/main/page.jsx
@@ -4,46 +4,50 @@ import Search from "./search/Search";
 import RoomList from "./rooms/RoomList";
 import { useSelector } from "react-redux";
 import CreateRoom from "../modal/room/create/create";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 export default function MainPage() {
   const [roomList, setRoomList] = useState([]); //오타있어서 수정해드렸어여
-  const {handle} = useSelector((state) => state.user.user);
+  const { handle } = useSelector((state) => state.user.user);
   const [show, setShow] = useState(false);
 
   useEffect(() => {
     //TODO roomList 받아오기
-    socket.on("gameRooms", (rooms) => {
-      setRoomList(rooms);
-    });
-
+    socket.emit("getsRooms");
     return () => {
       socket.off("gameRooms");
-    }
+    };
   }, []);
-
+  socket.on("getsRooms", (rooms) => {
+    console.log(rooms);
+    setRoomList(rooms);
+  });
   const cancelShow = useCallback(() => {
     setShow(false);
-  },[])
+  }, []);
 
   const onClickCreateRoom = useCallback(() => {
-    if(!handle){
+    if (!handle) {
       alert("로그인하여 주세요!");
       return;
     }
     setShow(true);
-  }, [handle])
+  }, [handle]);
 
   return (
     <div className="mainPage">
       <Search />
       <RoomList roomList={roomList} />
-      <div className="createRoom" onClick={()=>onClickCreateRoom()}>방 생성</div>
-      {show ? 
-        <CreateRoom show={show} cancelShow={cancelShow}></CreateRoom> 
-      : <></>}
+      <div className="createRoom" onClick={() => onClickCreateRoom()}>
+        방 생성
+      </div>
+      {show ? (
+        <CreateRoom show={show} cancelShow={cancelShow}></CreateRoom>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
