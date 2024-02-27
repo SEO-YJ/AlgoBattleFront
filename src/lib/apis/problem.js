7b78fbc76e4dc5332bd796f945523108aa8fb208
kim hyun soo <hyjkl0124@naver.com>
2024-02-27 20:40:46 +0900

    feat ProblemApi

--- src/lib/apis/problem.js
+++ src/lib/apis/problem.js
@@ -2,9 +2,14 @@ import instance from "./base";
 
 export async function getProblem(queryString, roomTier, users) {
   try {
-    const data = await instance.get(`/problem/${roomTier}${queryString}`, users);
+    const data = await instance.get(`/problem/${roomTier}${queryString}`, {
+      params: {
+        body: users,
+      },
+    });
+    console.log(users);
     return data;
   } catch (err) {
-    return Promise.reject(err); 
+    return Promise.reject(err);
   }
 }
